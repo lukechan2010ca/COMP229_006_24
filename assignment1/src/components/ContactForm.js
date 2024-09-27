6f1769e1c9cfd0433fa0154c55d72698bb20685c
lukechan2010ca <lukechan2010ca@gmail.com>
2024-09-27 01:32:06 -0400

    updated assignment 1

--- assignment1/src/components/ContactForm.js
+++ assignment1/src/components/ContactForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function ContactForm() {
+const ContactForm = () => {
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
