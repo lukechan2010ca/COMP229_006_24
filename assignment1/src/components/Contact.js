7bd12ecafbca1fb0b4f707001d9656a48abe8eed
lukechan2010ca <lukechan2010ca@gmail.com>
2024-09-28 22:37:29 -0400

    Assignment1 V1.2
    little adjustment

--- assignment1/src/components/Contact.js
+++ assignment1/src/components/Contact.js
@@ -5,7 +5,6 @@
 // Date: 28/9/2024
 // 
 
-
 // Section: import components
 import ContactForm from './ContactForm.js';
 
@@ -14,23 +13,22 @@ import ContactForm from './ContactForm.js';
 const Contact = () => {
   return (
     <div className="contact">
-      <h1>Contact Me</h1>
+      <h1>My Contact</h1>
       <div className="contact-content">
         <div className="contact-info">
-          <h2>Let's talk</h2>
           <p>
-            Feel free to reach out to me for any inquiries or collaborations.<br />I'm always excited
-            to work on new projects and meet fellow developers.
+            Feel free to contact for any inquiries or collaborations.<br />
+            I'm always excited to work on new projects and meet fellow developers.
           </p>
           <div className="card">
-            <h3>Contact Information</h3>
+            <h2>Contact Information</h2>
             <p>Email: cchan314@my.centennialcollege.ca</p>
             <p>Phone: (123) 456-7890</p>
             <p>Location: Ontario, Canada</p>
           </div>
         </div>
         <div className="contact-form-outter">
-          <h2>Send Me a Message</h2>
+          <h3>Send Me a Message</h3>
           <ContactForm />
         </div>
       </div>
